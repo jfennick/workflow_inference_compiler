3a22ce95cee64820066ab6b902c23e2bc641334b
Jake Fennick <jake.fennick@axleinfo.com>
2022-06-25 13:45:07 -1000

    added docking

---
+++ examples/data/download_data.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+wget --no-clobber https://scs.illinois.edu/system/files/inline-files/NCIDiversity.rar
+wget --no-clobber https://scs.illinois.edu/system/files/inline-files/NCINaturalChallenge.rar
+wget --no-clobber https://scs.illinois.edu/system/files/inline-files/NCIOpen.rar
+
+tar -xf NCIDiversity.rar
+tar -xf NCINaturalChallenge.rar
+tar -xf NCIOpen.rar
